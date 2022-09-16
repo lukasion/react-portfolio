2a1e533acc2a12cc159e46bcfb89d4ac33c031eb
ellukasion <lukasion@gmail.com>
2022-09-16 19:55:09 +0200

    Tweak tailwind.css

---
+++ src/pages/main.js
@@ -0,0 +1,9 @@
+const Main = () => {
+    return (
+        <div className="md:container mx-auto">
+            asd
+        </div>
+    )
+}
+
+export default Main
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,5 +15,30 @@ module.exports = {
             opacity: ['disabled'],
         },
     },
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        function ({ addComponents }) {
+            addComponents({
+                '.container': {
+                    maxWidth: '100%',
+                    '@screen sm': {
+                        maxWidth: '600px',
+                    },
+                    '@screen md': {
+                        maxWidth: '700px',
+                    },
+                    '@screen lg': {
+                        maxWidth: '800px',
+                    },
+                    '@screen xl': {
+                        maxWidth: '1024px',
+                    },
+                }
+            })
+        }
+    ],
+    corePlugins: {
+        container: false
+    },
+
 }
